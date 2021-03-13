4c6802bfbdafba00007cfa52b1f4173581790d25
Rafael Assad <rafaassadeng@gmail.com>
2021-03-13 10:55:36 -0300

    estou subindo porque mudei

--- src/pages/createDeck.js
+++ src/pages/createDeck.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { minionRequest } from '../services/cardsRequest';
+/* import { minionRequest } from '../services/cardsRequest'; */
+import { cards } from '../data/minions.json';
 import HSCards from '../components/Cards';
 
 const CreateDeck = () => {
-  const [cards, setCards] = useState([]);
 
-  useEffect(() => {
+   const [minion, setMinion] = useState(cards)
+
+/*   useEffect(() => {
     minionRequest().then((resp) => {
-      setCards(Object.values(resp.cards));
+      setCard(Object.values(resp.cards));
       console.log(cards);
     });
-  }, []);
+  }, []); */ 
+
+  // useEffect & componentDidMount apenas pra requisição
 
   return (
     <section>
       <h1>Create Decks</h1>
       <div className="grid">
-        {cards.map((card, idx) => (
+        {minion.map((card, idx) => (
           <div key={idx}>
             <HSCards key={card.id} {...card} />
           </div>
